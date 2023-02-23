6bc2e27805bf148aaef52c0e6afe2349ff478ab1
Gustavo Drawlim <62090895+GDrawlim@users.noreply.github.com>
2023-02-22 23:09:36 -0300

    Add files via upload

---
+++ apiset.py
@@ -0,0 +1,15 @@
+import os 
+
+os.environ['OPENAI_API_KEY'] = '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'
+import openai
+
+# Retrieve the API key from the environment variable
+api_key = os.environ.get('OPENAI_API_KEY')
+
+# Set the API key for the openai package
+openai.api_key = api_key
+
+# Generate text using the GPT API
+prompt = "Hello, my name is"
+response = openai.Completion.create(engine="davinci", prompt=prompt, max_tokens=5)
+print(response.choices[0].text)
